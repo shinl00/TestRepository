647dd23067c473133bd29aad2d44ba5f9c5571b2
shininglin <shinl00@163.com>
2019-01-18 18:45:39 +0800

    A修改了Main.java

--- HelloProjet/src/Main.java
+++ HelloProjet/src/Main.java
@@ -5,6 +5,7 @@ public class Main {
     @Test
     public void testHelloWrold() {
         System.out.println("HelloWorld!");
-		System.out.println("我修改了本地!");
+		System.out.println("B：我修改了本地!");
+		System.out.println("A：那我也我修改了本地!");
     }
 }
