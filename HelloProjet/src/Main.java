dc3562492c9d7c225b618c62a427b83e6a9970cb
shininglin <shinl00@163.com>
2019-01-18 19:52:13 +0800

    在分支里修改了Main.java

--- HelloProjet/src/Main.java
+++ HelloProjet/src/Main.java
@@ -6,6 +6,6 @@ public class Main {
     public void testHelloWrold() {
         System.out.println("HelloWorld!");
 		System.out.println("我修改了本地!");
-		System.out.println("明天天气依然晴朗!");
+		System.out.println("分支hhh!");
     }
 }
